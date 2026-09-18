b063ccf55a475413c168098c9c09fa4b4da3fa03
vnptmaitatduy <maitatduy.work@gmail.com>
2026-09-18 09:56:40 +0700

    fix: prevent AppLogger from duplicating log handlers

--- src/main/java/com/salesmanager/util/AppLogger.java
+++ src/main/java/com/salesmanager/util/AppLogger.java
@@ -1,23 +1,29 @@
 package com.salesmanager.util;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class AppLogger {
+    private static final Map<String, AppLogger> INSTANCES = new ConcurrentHashMap<>();
+
     private final Logger logger;
 
     private AppLogger(Class<?> clazz) {
         this.logger = Logger.getLogger(clazz.getName());
         this.logger.setUseParentHandlers(false);
-        ConsoleHandler handler = new ConsoleHandler();
-        handler.setLevel(Level.ALL);
-        this.logger.addHandler(handler);
+        if (this.logger.getHandlers().length == 0) {
+            ConsoleHandler handler = new ConsoleHandler();
+            handler.setLevel(Level.ALL);
+            this.logger.addHandler(handler);
+        }
         this.logger.setLevel(Level.ALL);
     }
 
     public static AppLogger get(Class<?> clazz) {
-        return new AppLogger(clazz);
+        return INSTANCES.computeIfAbsent(clazz.getName(), key -> new AppLogger(clazz));
     }
 
     public void info(String message) {
